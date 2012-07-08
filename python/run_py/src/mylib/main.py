3b2bb4f35f3904cdd7fe335c13805dea33b30e9e
Yu Watanabe <yunabe.public@gmail.com>
2012-07-09 00:12:48 +0900

    Learned runpy module in Python.

---
+++ python/run_py/src/mylib/main.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+import sys
+
+import os
+
+def main():
+  print '------- main.py -------'
+  print sys.path
+  print os
+
+if __name__ == '__main__':
+  main()
