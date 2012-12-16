16b17f822b2b9ad7aa44ac90fe6af40cd190a2c2
Yu Watanabe <yunabe.public@gmail.com>
2012-12-16 16:00:22 +0900

    Made max_num_thread.cc simper.

--- cpp/thread/max_num_thread.cc
+++ cpp/thread/max_num_thread.cc
@@ -21,48 +21,24 @@ using std::vector;
 // /proc/sys/vm/max_map_count / 2 == max threads in one process.
 // Add 'vm.max_map_count = XXX' to /etc/sysctl.conf.
 
-class Barrier {
-public:
-  Barrier() : mutex_(PTHREAD_MUTEX_INITIALIZER),
-              cond_(PTHREAD_COND_INITIALIZER),
-              done_(false) {}
-
-  void Wait() {
-    pthread_mutex_lock(&mutex_);
-    while (!done_) {
-      pthread_cond_wait(&cond_, &mutex_);
-    }
-    pthread_mutex_unlock(&mutex_);
-  }
-
-  void Done() {
-    pthread_mutex_lock(&mutex_);
-    done_ = true;
-    pthread_cond_broadcast(&cond_);
-    pthread_mutex_unlock(&mutex_);
-  }
-
-private:
-  pthread_mutex_t mutex_;
-  pthread_cond_t cond_;
-  bool done_;
-};
+static pthread_mutex_t block_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* waiting(void* data) {
-  auto* barrier = static_cast<Barrier*>(data);
-  barrier->Wait();
+  pthread_mutex_lock(&block_mutex);
+  pthread_mutex_unlock(&block_mutex);
 }
 
 int main(int argc, char** argv) {
-  int num = 1 * 1000;
+  int num = 16 * 1000;
 
-  Barrier barrier;
+  pthread_mutex_lock(&block_mutex);
+  
   printf("Creating thread objects.\n");
   vector<pthread_t> threads(num);
   printf("Thread objects are created.\n");
   int max_index = -1;
   for (int i = 0; i < num; ++i) {
-    int err = pthread_create(&threads[i], NULL, &waiting, &barrier);
+    int err = pthread_create(&threads[i], NULL, &waiting, NULL);
     if (err != 0) {
       printf("Failed to create a thread. (i == %d)\n", i);
       break;
@@ -70,8 +46,8 @@ int main(int argc, char** argv) {
     max_index = i;
   }
   printf("All threads are started.\n");
-  barrier.Done();
-  printf("barrier.Done.\n");
+  pthread_mutex_unlock(&block_mutex);
+  printf("Released block_mutex.\n");
   for (int i = 0; i < max_index; ++i) {
     pthread_join(threads[i], NULL);
   }
