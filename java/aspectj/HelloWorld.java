76757e9dfe1fec89253fdc90dd91e9b5a78bf70a
Yu Watanabe <yunabe.public@gmail.com>
2013-02-10 16:29:52 +0900

    Load-time wearving AspectJ.

--- java/aspectj/HelloWorld.java
+++ java/aspectj/HelloWorld.java
@@ -3,12 +3,12 @@ public class HelloWorld {
     
   public static void main(String[] args) {
     System.out.println("start main.");
-    new HelloWorld().sayHello("world!");
+    new HelloWorld().sayHello();
     System.out.println("end main.");
   }
 
-  void sayHello(String name) {
-    System.out.println("Hello " + name);
+  void sayHello() {
+    System.out.println("Hello world!");
   }
 }
 
