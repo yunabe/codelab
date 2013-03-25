f108b9052178330fd7c6670e5286a9378c18b9e7
Yu Watanabe <yunabe.public@gmail.com>
2013-03-25 21:34:15 +0900

    Improved messages from Hook.

--- java/bcel/Hook.java
+++ java/bcel/Hook.java
@@ -5,8 +5,21 @@ public class Hook {
     Object run(Object[] args);
   }
 
+  static private String join(String sep, Object[] args) {
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < args.length; i++) {
+      if (i != 0) {
+        builder.append(sep);
+      }
+      builder.append(args[i].toString());
+    }
+    return builder.toString();
+  }
+
   static Object hook(String name, Runner runner, Object[] args) {
-    System.out.println("hook: name == " + name);
-    return runner.run(args);
+    Object returnVal = runner.run(args);
+    System.out.println(name + ": args == [" + join(", ", args) +
+                       "], : return: [" + returnVal + "]");
+    return returnVal;
   }
 }
