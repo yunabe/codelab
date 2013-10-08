97075cf1ef002ac8d42150edbbfa6ce3c6f887a5
Yu Watanabe <yunabe.public@gmail.com>
2013-10-08 09:14:59 +0900

    Learned new features of C++ 11 a little.

--- cpp/cpp11/main.cc
+++ cpp/cpp11/main.cc
@@ -1,4 +1,62 @@
 #include <stdio.h>
+#include <vector>
+
+using std::vector;
+
+void iteration() {
+  // initialization list
+  vector<int> list{1, 2, 3};
+  // You can get rid of a whitespace between > and > in C++ 11.
+  vector<vector<int> > nested{{1}, {2, 3}, {4, 5, 6}};
+
+  // range based for
+  for (int val : list) {
+    printf("list[?] == %d\n", val);
+  }
+  for (auto& entry : nested) {
+    printf("(len(entry), entry[0]) == (%zd, %d)\n", entry.size(), entry[0]);
+  }
+}
+
+void null_pointer() {
+  int* p = nullptr;
+  int* q = NULL;
+
+  // compile error
+  // int r = nullptr;
+  // OK
+  int s = NULL;
+
+  // compile error
+  // s == nullptr;
+  // OK
+  s == NULL;
+}
+
+// unscoped enum
+enum OldEnum {VAL0, VAL1};
+
+// scoped enum. "class" can be "struct".
+enum class NewEnum {VAL0, VAL1};
+
+enum class  ScopedEnumWithType : long long {VAL0, VAL1};
+enum UnscopedEnumWithType : long long {VAL2, VAL3};
+
+// Forward declaration with type.
+enum class ScopedDeclaredEnum: int;  // ": int" is optional.
+enum UnscopedDeclaredEnum: int;
+
+void enum_cpp11() {
+  printf("VAL0: %d\n", VAL0);
+  printf("VAL1: %d\n", VAL1);
+  printf("NewEnum::VAL0: %d\n", NewEnum::VAL0);
+  printf("NewEnum::VAL1: %d\n", NewEnum::VAL1);
+
+  // OK. VAL0 is int.
+  if (VAL0 == 0) {}
+  // compile error
+  // if (NewEnum::VAL0 == 0) {}
+}
 
 int main(int argc, char** argv) {
   int x = 2;
@@ -7,5 +65,9 @@ int main(int argc, char** argv) {
   x = 3;
   printf("y == %d\n", y);
   printf("z == %d\n", z);
+
+  iteration();
+  null_pointer();
+  enum_cpp11();
   return 0;
 }
