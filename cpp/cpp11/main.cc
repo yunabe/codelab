06546b09f41fab7bff4ba55757953ecc55a85be4
Yu Watanabe <yunabe.public@gmail.com>
2013-10-08 10:42:43 +0900

    Learned unique_ptr in C++.

--- cpp/cpp11/main.cc
+++ cpp/cpp11/main.cc
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
 #include <map>
+#include <memory>  // auto_ptr, unique_ptr
 #include <string>
 #include <vector>
 
+using std::auto_ptr;
 using std::vector;
 using std::map;
 using std::string;
+using std::unique_ptr;
+
 
 void iteration() {
   // initialization list
@@ -68,6 +72,44 @@ void enum_cpp11() {
   // if (NewEnum::VAL0 == 0) {}
 }
 
+void unique_pointer() {
+  unique_ptr<int> up0;
+  up0.reset(new int);
+  // compile error
+  // unique_ptr<int> up1(up0);
+  unique_ptr<int> up1(std::move(up0));
+  // We need to move the pointer explicitly :)
+  printf("up0.get() == %p\n", up0.get());
+  printf("up1.get() == %p\n", up1.get());
+
+  vector<unique_ptr<int> > uni_vec;
+  uni_vec.push_back(unique_ptr<int>(new int(10)));
+  uni_vec.emplace_back(new int(20));
+  // compile error!
+  // uni_vec.push_back(new int(10));
+
+  // FYI: auto_ptr. auto_ptr is deprecated.
+  auto_ptr<int> ap0;
+  ap0.reset(new int);
+  auto_ptr<int> ap1(ap0);  // This moves the pointer from ap0 to ap1.
+  // std::move style is also fine in C++ 11 though auto_ptr is deprecated.
+  // auto_ptr<int> ap1(std::move(ap0));
+  printf("ap0.get() == %p\n", ap0.get());
+  printf("ap1.get() == %p\n", ap1.get());
+
+  // FYI: auto_ptr in containers (anyway, auto_ptr is deprecated.)
+  vector<auto_ptr<int> > auto_vec;
+  // compile error in C++03 but compilable in C++ 11
+  // (because push_back receive T&&?).
+  auto_vec.push_back(auto_ptr<int>(new int (10)));
+
+  // compile error in C++11 too.
+  // auto_vec.push_back(ap1);
+  auto_vec.push_back(std::move(ap1));
+
+  printf("ap1.get() == %p\n", ap1.get());  // (nil)
+}
+
 int main(int argc, char** argv) {
   int x = 2;
   auto y = x;  // int y = x;
@@ -79,5 +121,6 @@ int main(int argc, char** argv) {
   iteration();
   null_pointer();
   enum_cpp11();
+  unique_pointer();
   return 0;
 }
