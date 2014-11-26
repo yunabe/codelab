6ca5472f954dc010fed7ad527168386f59c524b2
Yu Watanabe <yunabe.public@gmail.com>
2014-11-26 23:10:00 +0900

    Added an boiler plate of commandline tool project.

---
+++ objectivec/ObjectiveCBasics/ObjectiveCBasics/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectiveCBasics
+//
+//  Created by yunabe on 2014/11/26.
+//  Copyright (c) 2014年 yunabe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
