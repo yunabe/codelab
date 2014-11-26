969d524336afe8542782168fe8094f1ea7077a3c
Yu Watanabe <yunabe.public@gmail.com>
2014-11-26 23:33:31 +0900

    Added YNBPerson to ObjectiveCBasics.

---
+++ objectivec/ObjectiveCBasics/ObjectiveCBasics/YNBPerson.h
@@ -0,0 +1,13 @@
+//
+//  YNBPerson.h
+//  ObjectiveCBasics
+//
+//  Created by yunabe on 2014/11/26.
+//  Copyright (c) 2014年 yunabe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YNBPerson : NSObject
+
+@end
---
+++ objectivec/ObjectiveCBasics/ObjectiveCBasics/YNBPerson.m
@@ -0,0 +1,13 @@
+//
+//  YNBPerson.m
+//  ObjectiveCBasics
+//
+//  Created by yunabe on 2014/11/26.
+//  Copyright (c) 2014年 yunabe. All rights reserved.
+//
+
+#import "YNBPerson.h"
+
+@implementation YNBPerson
+
+@end
