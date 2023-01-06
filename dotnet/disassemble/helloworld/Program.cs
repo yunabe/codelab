e273870581ff36e3ddbc805c71817b93d359670c
Yu Watanabe <yunabe.public@gmail.com>
2023-01-06 14:20:38 +0000

    Main in a static class.

--- dotnet/disassemble/helloworld/Program.cs
+++ dotnet/disassemble/helloworld/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace helloworld;
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
