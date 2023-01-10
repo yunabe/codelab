c2a6b21dbdfb27f4f955f577a89c4e38a1bed6a6
Yu Watanabe <yunabe.public@gmail.com>
2023-01-10 14:51:03 +0000

    Add a simple function to helloworld.

--- dotnet/disassemble/helloworld/Program.cs
+++ dotnet/disassemble/helloworld/Program.cs
@@ -1,8 +1,16 @@
 ﻿namespace helloworld;
 class Program
 {
+    private static long NaiveFib(long n) {
+        if (n <= 1) {
+            return n;
+        }
+        return NaiveFib(n - 1) + NaiveFib(n - 2);
+    }
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        long n = 40;
+        Console.WriteLine("NaiveFib({0}) = {1}", n, NaiveFib(n));
     }
 }
